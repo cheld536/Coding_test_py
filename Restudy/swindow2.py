a0cda54866a53531285b9afa6c264557b850cff7
Yunhui <kss7035@naver.com>
2024-10-29 17:17:21 +0900

    #swindow2 주석 수정

--- Restudy/swindow2.py
+++ Restudy/swindow2.py
@@ -25,9 +25,9 @@ now = list(map(int, input().split()))
 
 #새로운 값이 들어올 때마다 정렬 대신 현재 수보다 큰 값을 덱에서 제거해 시간 복잡도를 줄임
 for i in range(N):
-    while mydeque and mydeque[-1][0] > now[i]:
-        mydeque.pop()
-    mydeque.append((now[i],i))
-    if mydeque[0][1] <= i -L: #범위에서 벗어난 값은 덱에서 제거
+    while mydeque and mydeque[-1][0] > now[i]: # deque 제일 왼쪽에 있는 파일보다 now[i]가 클때 까지
+        mydeque.pop()                           # 오른쪽에 있는 자료를 pop
+    mydeque.append((now[i],i))                  # now[i] 값과, i 번째 데이터라고 알리고 입력
+    if mydeque[0][1] <= i - L: #범위에서 벗어난 값은 덱에서 제거
         mydeque.popleft()
     print(mydeque[0][0],end='')
